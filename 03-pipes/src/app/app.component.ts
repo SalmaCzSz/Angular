44242ea50564839336778f528d0a6dcc1523fb5f
Salma Cruz Sánchez <salma.cruz@fygdevelopers.com>
2023-12-15 17:08:06 -0600

    15/12/2023 | Aplicación 03 - Pipes

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre: string = 'Capitán América';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  personajes: string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
+  valorPromesa = new Promise<string> ((resolve) => {
+    setTimeout(() => {
+      resolve('llegó la data');
+    }, 4500);
+  });
+
+  fecha: Date = new Date();
+  idioma: string = 'es';
+}
